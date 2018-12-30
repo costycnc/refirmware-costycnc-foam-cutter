6bf31564523517e179b1e5074e354215bb0afb2c
Boboaca Costel <bobyca2003@yahoo.com>
2018-12-30 17:21:12 +0100

    Update refirmware_costycnc.bat

--- refirmware_costycnc.bat
+++ refirmware_costycnc.bat
@@ -5,6 +5,6 @@ set /p UserInputPath= Insert your port where costycnc is connected (example com1
 
 mode %UserInputPath% BAUD=9600 PARITY=n DATA=8
     echo E > %UserInputPath%
-bin\avrdude -p m328p -b 57600 -P %UserInputPath% -c arduino -U flash:w:refirmware.hex 
+bin\avrdude -p m328p -b 57600 -P %UserInputPath% -c arduino -U flash:w:bin\refirmware.hex 
 
 pause 
